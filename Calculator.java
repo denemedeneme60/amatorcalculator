d781edfc4648a5d4d32a757aa2c74fe849efe129
Ahmet Burak KILIÇKAYA <a.burakilickaya@gmail.com>
2023-02-09 21:49:26 +0300

    çarpma methodu

--- Calculator.java
+++ Calculator.java
@@ -7,6 +7,14 @@ public class Calculator {
         
 
     }
+    // Ahmet --> ÇARPMA 
+    public static double carpma(int integer1, int integer2){
+        
+        int result= integer1*integer2;
+
+        return result;
+
+    }
 
     
 }
